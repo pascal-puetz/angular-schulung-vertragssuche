fc4b69f445f64d8ee31df57a9a06b7746dd1e5f0
Pascal Pütz <pascal.puetz@generali.com>
2018-11-08 12:55:28 +0100

    Adds animation example

--- src/app/components/table-line/table-line.component.ts
+++ src/app/components/table-line/table-line.component.ts
@@ -1,9 +1,22 @@
+import {animate, state, style, transition, trigger} from '@angular/animations';
 import {Component, Input} from '@angular/core';
 
 @Component({
   selector: 'gen-table-line',
   templateUrl: './table-line.component.html',
-  styleUrls: ['./table-line.component.scss']
+  styleUrls: ['./table-line.component.scss'],
+  animations: [
+    trigger('scale', [
+      state('small', style({
+        transform: 'scale(1)',
+      })),
+      state('large', style({
+        transform: 'scale(1.03)',
+      })),
+      transition('small => large', animate('100ms ease-in')),
+      transition('large => small', animate('100ms ease-out'))
+    ]),
+  ]
 })
 export class TableLineComponent {
 
@@ -16,5 +29,8 @@ export class TableLineComponent {
    * A flag indicating if this row is even or odd
    */
   @Input() public even: boolean = false;
+
+
+  public scaleState:'small' | 'large' = 'small';
 
 }
